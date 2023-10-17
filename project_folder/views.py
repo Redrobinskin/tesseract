ee4eb5e9bab2c9bf7e41cbad5e7cbb6e78bdeea5
Redrobinskin <142557343+Redrobinskin@users.noreply.github.com>
2023-10-18 07:53:30 +1100

    Revert "Add the missing template "dashboard.html" to fix the TemplateNotFound error in the web UI."

--- project_folder/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-def dashboard_view():
-    try:
-        return render_template('dashboard.html')
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return render_template('error.html', error=e)
